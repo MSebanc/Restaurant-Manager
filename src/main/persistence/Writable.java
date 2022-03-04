ed0cc6de5bbb4c5d57942586f2318b0d9c0fb59f
mjsebanc <mjsebanc@students.cs.ubc.ca>
2022-03-04 00:00:38 -0800

    Credited JsonSerializationDemo for Writable.java

--- src/main/persistence/Writable.java
+++ src/main/persistence/Writable.java
@@ -2,7 +2,7 @@ package persistence;
 
 import org.json.JSONObject;
 
-// Interface for toJson() command
+// Interface for toJson() command: Code structure is based on the project JsonSerializationDemo
 public interface Writable {
     // EFFECTS: returns this as JSON object
     JSONObject toJson();
